e86a8d9b6014e2b99d9a7806dccf5bd46edfd2d9
angeletal <martinez.reina.angel.fernando@iescamas.es>
2022-11-30 14:24:06 +0100

    todoSubido

---
+++ config.php
@@ -0,0 +1,30 @@
+<?php  // Moodle configuration file
+
+unset($CFG);
+global $CFG;
+$CFG = new stdClass();
+
+$CFG->dbtype    = 'mariadb';
+$CFG->dblibrary = 'native';
+$CFG->dbhost    = 'localhost';
+$CFG->dbname    = 'moodle16';
+$CFG->dbuser    = 'myangeletae2';
+$CFG->dbpass    = 'DOIxXHN6';
+$CFG->prefix    = 'mdl_';
+$CFG->dboptions = array (
+  'dbpersist' => 0,
+  'dbport' => 3306,
+  'dbsocket' => '',
+  'dbcollation' => 'utf8_general_ci',
+);
+
+$CFG->wwwroot   = 'http://www.angeletal.com.mialias.net/moodle';
+$CFG->dataroot  = '/usr/home/angeletal.com/entrada';
+$CFG->admin     = 'admin';
+
+$CFG->directorypermissions = 0777;
+
+require_once(__DIR__ . '/lib/setup.php');
+
+// There is no php closing tag in this file,
+// it is intentional because it prevents trailing whitespace problems!
